64e724656b473d9247d3f91c6eb5fe1ffe479d42
chuphuhai1993 <chuphuhai1993@gmail.com>
2025-12-19 15:35:29 +0700

    fix favicon and 404 path for custom domain

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,8 +9,8 @@ const Layout = ({ children }) => {
                 <title>wwsmind</title>
                 <meta name='description' content='wwsmind making it simple for you to build and grow your SaaS applications, or any business idea' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' type='image/png' href='/web_wwsmind/favicon.png' />
-                <link rel='apple-touch-icon' href='/web_wwsmind/favicon.png' />
+                <link rel='icon' type='image/png' href='/favicon.png' />
+                <link rel='apple-touch-icon' href='/favicon.png' />
             </Head>
             <Navbar />
             <main>{children}</main>
